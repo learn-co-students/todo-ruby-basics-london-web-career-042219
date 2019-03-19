8da1f3696199fa8a1d9cd8e65802875c3e4820f6
Danielle Mizrachi <dmiz@brandeis.edu>
2019-03-19 21:27:21 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
-def division(num1, num2)
- 
+def division(num1,num2)
+num1/num2
 end
 
 def assign_variable(value)
-  
+  value="Bob"
 end
 
-def argue
-  
+def argue(argument)
+argument
 end
 
-def greeting
-
+def greeting(greeting,name)
+  "#{greeting}, #{name}"
 end
 
 def return_a_value
-
+phrase= "Nice"
 end
 
 def last_evaluated_value
-
+value="expert"
 end
 
-def pizza_party
-
+def pizza_party (pizza="cheese")
+puts pizza
+phrase=pizza
 end
